a73eb652ea5e7f0424afe38b7310399f5e3dd5dc
albertliu <xiaochuan_liu@hotmail.com>
2021-06-09 09:10:57 +0800

    redis expire

--- utils/redisdb.js
+++ utils/redisdb.js
@@ -2,14 +2,14 @@ const redisConfig = {
     appID : "elearning",
     session_secret : 'Asecret123-ShElearning2020',
     cookie : {
-        maxAge : 10 * 60 * 1000     //user page cookie expire(ms)  10 minutes
+        maxAge : 10 * 24 * 60 * 60 * 1000     //user page cookie expire(ms)  10 days
     },
     sessionStore : {
         host : "127.0.0.1",
         port : "6379",
         pass : "ad08Shi93!asT",
         db : 1,
-        ttl : 1 * 60 * 60,       //redis store session expire(s) 1 hour
+        ttl : 24 * 60 * 60,       //redis store session expire(s) 24 hour
         logErrors : true
     }
 };
