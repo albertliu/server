3114f45d5192f451ee6e50ebab2d6ecd94360370
albertliu <xiaochuan_liu@hotmail.com>
2021-12-24 15:01:49 +0800

    C30/C31

--- routes/files.js
+++ routes/files.js
@@ -731,7 +731,7 @@ router.post('/generate_diploma_byClassID', function(req, res, next) {
         let pages = [];
         let paths = [];
         certID = data1.recordset[0]["certID"];
-        if(certID=="C2" || certID == "C18" || certID == "C19"){
+        if(certID=="C30" || certID=="C31" || certID == "C18" || certID == "C19"){
           certID = "C2";
         }else{
           pW1 = '280mm';
