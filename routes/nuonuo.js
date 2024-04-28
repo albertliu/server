8a44e489c4134029fb53a2ad14677be8f5b420c1
albertliu <xiaochuan_liu@hotmail.com>
2024-04-28 11:06:00 +0800

    oderInvoiceReturn

--- routes/nuonuo.js
+++ routes/nuonuo.js
@@ -10,7 +10,7 @@ var response, sqlstr, params;
 
 //支付回调接口
 router.post('/oderPaymentReturn', function(req, res, next) {
-  console.log("body:", req.body, "query:", req.query);
+  console.log("oderPaymentReturn body:", req.body, "query:", req.query);
   let hexData = req.body.data;
   let encryptedHexStr  = crypto.enc.Hex.parse(hexData);
   let encryptedBase64Str  = crypto.enc.Base64.stringify(encryptedHexStr);
@@ -24,21 +24,34 @@ router.post('/oderPaymentReturn', function(req, res, next) {
 });
 
 //退款回调接口
-router.post('/nuonuo/oderRefundReturn', function(req, res, next) {
-  sqlstr = "select * from dbo.getCourseListByCertID('" + req.query.certID + "')";
-  params = {};
-  //console.log("params:", params);
-  db.excuteSQL(sqlstr, params, function(err, data){
-    if (err) {
-      console.log(err);
-      let response = {"status":9};
-      return res.send(response);
-    }
-    response = data.recordset;
-    return res.send(response);
+router.post('/oderRefundReturn', function(req, res, next) {
+  console.log("oderRefundReturn body:", req.body, "query:", req.query);
+  let hexData = req.body.data;
+  let encryptedHexStr  = crypto.enc.Hex.parse(hexData);
+  let encryptedBase64Str  = crypto.enc.Base64.stringify(encryptedHexStr);
+  let decryptedData  = crypto.AES.decrypt(encryptedBase64Str, key, {
+      mode: crypto.mode.ECB,
+      padding: crypto.pad.Pkcs7
   });
+  let text = decryptedData.toString(crypto.enc.Utf8);
+  console.log(text);
+  return res.send(text);
 });
 
+//发票回调接口
+router.post('/oderInvoiceReturn', function(req, res, next) {
+  console.log("oderInvoiceReturn body:", req.body, "query:", req.query);
+  let hexData = req.body.data;
+  let encryptedHexStr  = crypto.enc.Hex.parse(hexData);
+  let encryptedBase64Str  = crypto.enc.Base64.stringify(encryptedHexStr);
+  let decryptedData  = crypto.AES.decrypt(encryptedBase64Str, key, {
+      mode: crypto.mode.ECB,
+      padding: crypto.pad.Pkcs7
+  });
+  let text = decryptedData.toString(crypto.enc.Utf8);
+  console.log(text);
+  return res.send(text);
+});
 
 
 module.exports = router;
